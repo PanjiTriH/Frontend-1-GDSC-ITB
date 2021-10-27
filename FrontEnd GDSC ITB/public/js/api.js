a60645f5968d5d3954c7d2e80ad2fa166f808fc6
strugglE <panjitri379@gmail.com>
2021-10-27 15:19:34 +0700

    Adding Commit File

---
+++ FrontEnd GDSC ITB/public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "4ec41752f687550eab90c8e7eba6352b";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
